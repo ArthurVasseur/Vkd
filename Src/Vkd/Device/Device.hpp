b11c129fdec8a6abe6e5b4688047ea860b786e69
Arthur Vasseur <arthur.vasseur@hotmail.fr>
2025-12-06 08:55:56 +0100

    feat: implement vkFlushMappedMemoryRanges and vkInvalidateMappedMemoryRanges
    Add stub implementations for FlushMappedMemoryRanges and InvalidateMappedMemoryRanges to fix CTS crash. For the software renderer using CPU memory, these functions are no-ops as CPU memory is already coherent.

--- Src/Vkd/Device/Device.hpp
+++ Src/Vkd/Device/Device.hpp
@@ -84,6 +84,8 @@ namespace vkd
 		static void VKAPI_CALL FreeMemory(VkDevice device, VkDeviceMemory memory, const VkAllocationCallbacks* pAllocator);
 		static VkResult VKAPI_CALL MapMemory(VkDevice device, VkDeviceMemory memory, VkDeviceSize offset, VkDeviceSize size, VkMemoryMapFlags flags, void** ppData);
 		static void VKAPI_CALL UnmapMemory(VkDevice device, VkDeviceMemory memory);
+		static VkResult VKAPI_CALL FlushMappedMemoryRanges(VkDevice device, uint32_t memoryRangeCount, const VkMappedMemoryRange* pMemoryRanges);
+		static VkResult VKAPI_CALL InvalidateMappedMemoryRanges(VkDevice device, uint32_t memoryRangeCount, const VkMappedMemoryRange* pMemoryRanges);
 
 		static VkResult VKAPI_CALL CreateGraphicsPipelines(VkDevice device, VkPipelineCache pipelineCache, uint32_t createInfoCount, const VkGraphicsPipelineCreateInfo* pCreateInfos, const VkAllocationCallbacks* pAllocator, VkPipeline* pPipelines);
 		static VkResult VKAPI_CALL CreateComputePipelines(VkDevice device, VkPipelineCache pipelineCache, uint32_t createInfoCount, const VkComputePipelineCreateInfo* pCreateInfos, const VkAllocationCallbacks* pAllocator, VkPipeline* pPipelines);
